89a58e4dc1bdd364f6604b5226dc340c436d47c6
dhoRam <dhondiram.jadhav@lntinfotech.com>
2020-08-13 15:34:41 +0530

    notification

--- src/app/mentor/mentor-dashboard/mentor-dashboard.component.ts
+++ src/app/mentor/mentor-dashboard/mentor-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CourseService } from 'src/app/course.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 type NewType = any;
 
@@ -16,7 +17,7 @@ export class MentorDashboardComponent implements OnInit {
   searchSub: Subscription;
   search: string;
   private sub: any;
-  constructor(private courseService: CourseService, private routes: ActivatedRoute, private route: Router) { }
+  constructor(private courseService: CourseService, private routes: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.loadData();
@@ -77,9 +78,13 @@ export class MentorDashboardComponent implements OnInit {
   }
   acceptProposal(course: any, type: string) {
     const reqBody = { user_id: course.user_id, mentor_id: course.mentor_id, technology_id: course.technology_id, proposalStatus: type };
+    const status = type === 'Started' ? 'approved' : 'rejected';
+    const msg = this.authService.getAuthUser().name + ' has ' + status + ' your proposal for the ' + course.technology + ' training.';
+    const obj = { user_id: course.user_id, description: msg, status: 0, created_on: new Date(), modified_on: new Date() };
     this.courseService.acceptProposal(reqBody).subscribe(res => {
       this.msgStatus.message = 'Proposal' + type + 'Successfully';
       this.msgStatus.status = false;
+      this.saveNotification(obj);
       this.loadData();
     }, error => {
       console.log('error', error);
@@ -91,6 +96,28 @@ export class MentorDashboardComponent implements OnInit {
       this.msgStatus.message = msg;
       this.msgStatus.type = false;
 
+    });
+  }
+  saveNotification(obj) {
+    console.log('obj', obj);
+    this.courseService.saveNotification(obj).subscribe(res => {
+      console.log('res==.', res);
+      this.msgStatus.message = '';
+      this.msgStatus.status = false;
+      if (!this.courseList.length) {
+        this.msgStatus.status = true;
+        this.msgStatus.message = 'Not record found !';
+        this.msgStatus.type = false;
+      }
+    }, error => {
+      console.log('error', error);
+      let msg = 'Oops !! Something went wrong, please contact the administrator';
+      if (error.error.message) {
+        msg = error.error.message;
+      }
+      this.msgStatus.status = true;
+      this.msgStatus.message = msg;
+      this.msgStatus.type = false;
     });
   }
 }
